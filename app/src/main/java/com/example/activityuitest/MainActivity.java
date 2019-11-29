ec41a4cb6f70c97eee53146a4704f3f9e2508550
Nancy-627 <w2433798615@qq.com>
2019-11-29 21:24:07 +0800

    six

--- app/src/main/java/com/example/activityuitest/MainActivity.java
+++ app/src/main/java/com/example/activityuitest/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.activityuitest;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1=(Button)findViewById(R.id.button01);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,ActivitySignIn.class);
+                startActivity(intent);
+            }
+        });
+        Button button4=(Button)findViewById(R.id.button04);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,ActivitySignUp.class);
+                startActivity(intent);
+            }
+        });
+        Button button2=(Button)findViewById(R.id.button02);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "新浪微博登陆", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button3=(Button)findViewById(R.id.button03);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "腾讯微博登陆", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
     }
 }
